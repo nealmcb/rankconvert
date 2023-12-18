b9774915467f04b3ddadb889722ed4877f799495
Neal McBurnett <neal@mcburnett.org>
2023-12-17 20:01:02 -0700

    clean up pairwise a bit

--- pairwise.py
+++ pairwise.py
@@ -1,6 +1,10 @@
 """
+Parse a .blt file of ranked choice cast vote records.
 Print counts of who wins between each pair of candidates
+Thanks to ChatGPT for a starting point
 """
+
+import sys
 
 def index_with_default(list, key, default=None):
     try:
@@ -14,7 +18,6 @@ def process_input(input_data):
     lines = lines[1:]  # Skip the first line
 
     # Initialize a dictionary to store the count of pairs
-    # TODO: consider all valid numbers
     candidates = range(1, n+1)
     pair_counts = {(i, j): 0 for i in candidates for j in candidates if i != j}
 
@@ -26,29 +29,12 @@ def process_input(input_data):
         numbers = numbers[1:]
         indices = [None] + [index_with_default(numbers, c, n+2) for c in candidates]
 
-        # go thru pairs and figure out which wins: lower index
-        #  Get index of each number, then for each pair look up the two and compare
-        # or go thru numbers and accumulate pairs with that number first, discarding matches with that number second
+        # go thru pairs and figure out if the first in the pair beat the other
 
         for first, second in pair_counts.keys():
             if indices[first] < indices[second]:
                 pair_counts[(first, second)] += 1
             # If they're equal, neither...
-
-            #import pdb; pdb.set_trace()
-
-    """
-        if len(numbers) == 1:
-            num = numbers[0]
-            for other_num in range(1, 5):
-                if num != other_num:
-                    pair_counts[(num, other_num)] += 1
-        else:
-            for i in range(0, len(numbers), 2):
-                pair = (numbers[i], numbers[i + 1] if i + 1 < len(numbers) else None)
-                if pair[1] is not None:
-                    pair_counts[pair] += 1
-    """
 
     return pair_counts
 
@@ -62,7 +48,6 @@ input_data = """
 ignore me
 """
 
-import sys
 input_data = open('output.blt').read()
 # input_data = sys.stdin.read()
 
